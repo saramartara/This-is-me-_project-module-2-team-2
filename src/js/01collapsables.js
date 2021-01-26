5ab5abb9c2d8f42a0fbc4a5a3990b84fe6ab2305
saramartara <saramartinezaragon@gmail.com>
2021-01-26 19:06:16 +0100

    update collapsable desing js

--- src/js/01collapsables.js
+++ src/js/01collapsables.js
@@ -3,7 +3,11 @@
 //la clase .js-slide se aplica al contenedor padre en la sección design.html <fliesdset>
 
 const collapsable = document.querySelector('.js-slide');
+const collapsableHeader = document.querySelector('.collapsable__header');
+// const collapsableContent = document.querySelector('.js-content');
+
 function show() {
   collapsable.classList.toggle('collapsable--close');
 }
-collapsable.addEventListener('click', show);
+
+collapsableHeader.addEventListener('click', show);
