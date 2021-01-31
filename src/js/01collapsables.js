eb2d1bcb867c4a896f5acfed4244277232eca5db
Sagramielgo <sagramielgo@gmail.com>
2021-01-31 23:42:01 +0100

    close collapsables, grow resetSection, rewrite html clases for form pastials

--- src/js/01collapsables.js
+++ src/js/01collapsables.js
@@ -1,9 +1,23 @@
 'use strict';
 
-const collapsable = document.querySelector('.js-slide');
-const collapsableHeader = document.querySelector('.collapsable__header');
+const collapsable1 = document.querySelector('.js-slide1');
+const collapsable2 = document.querySelector('.js-slide2');
+const collapsable3 = document.querySelector('.js-slide3');
 
-function show() {
-  collapsable.classList.toggle('collapsable--close');
+const designElement = document.querySelector('.js-design');
+const fillElement = document.querySelector('.js-fill');
+const shareElement = document.querySelector('.js-share');
+
+function handleShow1() {
+  collapsable1.classList.toggle('collapsable--open');
 }
-collapsableHeader.addEventListener('click', show);
+function handleShow2() {
+  collapsable2.classList.toggle('collapsable--open');
+}
+function handleShow3() {
+  collapsable3.classList.toggle('collapsable--open');
+}
+
+designElement.addEventListener('click', handleShow1);
+fillElement.addEventListener('click', handleShow2);
+shareElement.addEventListener('click', handleShow3);
