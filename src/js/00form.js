0830b345678766f024e1c5a2fd48c6e6324fdeab
saramartara <saramartinezaragon@gmail.com>
2021-02-02 13:30:56 +0100

    update 00form.js with function to reuse code

--- src/js/00form.js
+++ src/js/00form.js
@@ -1,92 +1,117 @@
 'use strict';
+const inputsTextConfig = [
+  {
+    inputClass: '.js-name',
+    cardClass: '.js-namePreview',
+    defaultValue: 'Nombre Apellido',
+    cardPrefix: '',
+    cardElementAttribute: 'innerHTML',
+  },
+  {
+    inputClass: '.js-position',
+    cardClass: '.js-positionPreview',
+    defaultValue: 'Front-end developer',
+    cardPrefix: '',
+    cardElementAttribute: 'innerHTML',
+  },
 
-//NOMBRE
-const nameElement = document.querySelector('.js-name');
-const namePreviewElement = document.querySelector('.js-namePreview');
+  {
+    inputClass: '.js-email',
+    cardClass: '.js-emailPreview',
+    defaultValue: '',
+    cardPrefix: 'mailto:',
+    cardElementAttribute: 'href',
+  },
+  {
+    inputClass: '.js-phone',
+    cardClass: '.js-phonePreview',
+    defaultValue: '',
+    cardPrefix: 'tel:',
+    cardElementAttribute: 'href',
+  },
+  {
+    inputClass: '.js-linkedin',
+    cardClass: '.js-linkedinPreview',
+    defaultValue: '',
+    cardPrefix: 'https://www.linkedin.com/in/',
+    cardElementAttribute: 'href',
+  },
+  {
+    inputClass: '.js-github',
+    cardClass: '.js-githubPreview',
+    defaultValue: '',
+    cardPrefix: 'https://github.com/',
+    cardElementAttribute: 'href',
+  },
+];
 
-function handleName(event) {
-  const nameValue = event.target.value;
-  if (nameValue === '') {
-    namePreviewElement.innerHTML = 'Nombre Apellido';
-  } else {
-    namePreviewElement.innerHTML = nameValue;
+function updateAllInputs() {
+  // recorro los 6 inputs del array inputsTextConfig
+  for (const inputTextConfig of inputsTextConfig) {
+    // por cada objeto del array inputsTextConfig hago:
+    console.log(
+      'Empiezo una nueva iteración del for con la configuración del elemento:',
+      inputTextConfig
+    );
+    // obtengo el elemento input, el origen
+    const inputElement = document.querySelector(inputTextConfig.inputClass);
+    console.log(
+      'Elemento del formulario:',
+      inputTextConfig.inputClass,
+      inputElement
+    );
+    // obtengo el elemento de la card, el destino
+    const cardElement = document.querySelector(inputTextConfig.cardClass);
+    console.log(
+      'Elemento del la tarjeta:',
+      inputTextConfig.cardClass,
+      cardElement
+    );
+    // obtengo el valor del input
+    let newValue = inputElement.value;
+
+    // compruebo si tengo que usar el innerHTML, es decir si es el nombre o la profesion
+    if (inputTextConfig.cardElementAttribute === 'innerHTML') {
+      // compruebo si está vacío
+      if (inputElement.value === '') {
+        newValue = inputTextConfig.defaultValue;
+      } else {
+        newValue = inputElement.value;
+      }
+      console.log('Valor por defecto:', inputTextConfig.defaultValue);
+      console.log('Nuevo valor a poner en la tarjeta:', newValue);
+      // actualizo la tarjeta
+      cardElement.innerHTML = newValue;
+    }
+    // si no es de tipo innerHTML, compruebo si tengo que usar el href, es decir si es el email, phone, linkedin o gihtub
+    else if (inputTextConfig.cardElementAttribute === 'href') {
+      // compruebo si está vacío
+      if (inputElement.value === '') {
+        newValue = '#';
+      } else {
+        // limpio el prefijo por si acaso la usuaria ha escrito cosas como:
+        // - https://github.com/migueldelmazo en vez de migueldelmazo a secas
+        // - https://www.linkedin.com/in/migueldelmazo en vez de migueldelmazo a secas
+        newValue = newValue.replace(inputTextConfig.cardPrefix, '');
+        // vuelvo a añadir el prefijo https://www.linkedin.com/in/
+        newValue = inputTextConfig.cardPrefix + newValue;
+      }
+      console.log('Valor del prefijo:', inputTextConfig.cardPrefix);
+      console.log('Nuevo valor a poner en la tarjeta:', newValue);
+      // actualizo la tarjeta
+      cardElement.href = newValue;
+    }
+    console.log('-------------------------------------------------');
   }
 }
 
-nameElement.addEventListener('keyup', handleName);
-
-//PUESTO
-const positionElement = document.querySelector('.js-position');
-const positionPreviewElement = document.querySelector('.js-positionPreview');
-
-function handlePosition(event) {
-  const positionValue = event.target.value;
-
-  if (positionValue === '') {
-    positionPreviewElement.innerHTML = 'Front-end developer';
-  } else {
-    positionPreviewElement.innerHTML = positionValue;
-  }
+// obtengo los 6 inputs del html
+const inputTextElements = document.querySelectorAll('.js-inputText');
+// escucho a cada uno de ellos con un addEventListener
+for (const inputTextElement of inputTextElements) {
+  inputTextElement.addEventListener('keyup', updateAllInputs);
 }
 
-positionElement.addEventListener('keyup', handlePosition);
-
-//IMAGEN DE PERFIL
-
-//EMAIL
-
-const emailElement = document.querySelector('.js-email');
-const emailPreviewElement = document.querySelector('.js-emailPreview');
-function handleEmail(ev) {
-  const emailValue = ev.target.value;
-
-  if (emailValue === '') {
-    emailPreviewElement.href = '';
-  } else {
-    emailPreviewElement.href = 'mailto:' + emailValue;
-  }
-}
-emailElement.addEventListener('keyup', handleEmail);
-
-//TELEFONO
-const phoneElement = document.querySelector('.js-phone');
-const phonePreviewElement = document.querySelector('.js-phonePreview');
-function handlePhone(event) {
-  const phoneValue = event.target.value;
-
-  if (phoneValue === '') {
-    phonePreviewElement.href = '';
-  } else {
-    phonePreviewElement.href = 'tel:' + phoneValue;
-  }
-}
-
-phoneElement.addEventListener('keyup', handlePhone);
-
-//LINKEDIN
-const linkedinElement = document.querySelector('.js-linkedin');
-const linkedinPreviewElement = document.querySelector('.js-linkedinPreview');
-function handlelinkedin(ev) {
-  const linkedinValue = ev.target.value;
-  if (linkedinValue === '') {
-    linkedinPreviewElement.href = '';
-  } else {
-    linkedinPreviewElement.href = linkedinValue;
-  }
-}
-linkedinElement.addEventListener('keyup', handlelinkedin);
-
-//GITHUB
-const githubElement = document.querySelector('.js-github');
-const githubPreviewElement = document.querySelector('.js-githubPreview');
-
-function handleGithub(ev) {
-  const githubValue = ev.target.value;
-
-  if (githubValue === '') {
-    githubPreviewElement.href = '';
-  } else {
-    githubPreviewElement.href = githubValue;
-  }
-}
-githubElement.addEventListener('keyup', handleGithub);
+// al arrancar la página proceso todos los inputs
+// como updateAllInputs no recibe eventos puedo ejecutar esta función al inicio, tras un evento de usuaria o cuando me de la real gana!!!
+updateAllInputs();
